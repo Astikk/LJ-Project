13a7a4364e8a21446a4187a8bd71e50aa6865432
Astik singh <lusiferr2001@gmail.com>
2021-08-14 11:41:17 +0530

    Done By Astik Singh

---
+++ script.js
@@ -0,0 +1,52 @@
+//                             Header 
+ 
+ function setnew1(){
+    document.getElementsByClassName("same1")[0].style.width="121px"};
+
+function setold1(){
+   document.getElementsByClassName("same1")[0].style.width="110px"};
+
+
+
+
+   function setnew2(){
+    document.getElementsByClassName("same2")[0].style.width="110px"}
+
+function setold2(){
+   document.getElementsByClassName("same2")[0].style.width="100px"}
+
+
+
+
+   function setnew3(){
+    document.getElementsByClassName("same3")[0].style.width="110px"}
+
+function setold3(){
+   document.getElementsByClassName("same3")[0].style.width="100px"}
+
+
+
+   function setnew4(){
+    document.getElementsByClassName("same4")[0].style.width="129px"}
+
+function setold4(){
+   document.getElementsByClassName("same4")[0].style.width="120px"}
+
+
+
+
+   function setnew5(){
+    document.getElementsByClassName("same5")[0].style.width="211px"}
+
+function setold5(){
+   document.getElementsByClassName("same5")[0].style.width="200px"}
+
+
+
+
+   function setnew6(){
+    document.getElementsByClassName("same6")[0].style.width="129px"}
+
+function setold6(){
+   document.getElementsByClassName("same6")[0].style.width="120px"}
+
