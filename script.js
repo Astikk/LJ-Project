9c44597da7af8abce6e8c9364b415f9e10e811c4
Astik singh <lusiferr2001@gmail.com>
2021-08-17 18:08:09 +0530

    best default web

--- script.js
+++ script.js
@@ -1,52 +1,16 @@
-//                             Header 
- 
- function setnew1(){
-    document.getElementsByClassName("same1")[0].style.width="121px"};
 
-function setold1(){
-   document.getElementsByClassName("same1")[0].style.width="110px"};
+document.querySelector('#account').addEventListener('click', function(){
 
+   document.querySelector('.bg-model').style.display = 'grid'
 
+   
 
-
-   function setnew2(){
-    document.getElementsByClassName("same2")[0].style.width="110px"}
-
-function setold2(){
-   document.getElementsByClassName("same2")[0].style.width="100px"}
+});
 
 
+document.querySelector('.close').addEventListener('click', function(){
+
+   document.querySelector('.bg-model').style.display = 'none'
 
 
-   function setnew3(){
-    document.getElementsByClassName("same3")[0].style.width="110px"}
-
-function setold3(){
-   document.getElementsByClassName("same3")[0].style.width="100px"}
-
-
-
-   function setnew4(){
-    document.getElementsByClassName("same4")[0].style.width="129px"}
-
-function setold4(){
-   document.getElementsByClassName("same4")[0].style.width="120px"}
-
-
-
-
-   function setnew5(){
-    document.getElementsByClassName("same5")[0].style.width="211px"}
-
-function setold5(){
-   document.getElementsByClassName("same5")[0].style.width="200px"}
-
-
-
-
-   function setnew6(){
-    document.getElementsByClassName("same6")[0].style.width="129px"}
-
-function setold6(){
-   document.getElementsByClassName("same6")[0].style.width="120px"}
-
+});
